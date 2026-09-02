74ddc5ff5df280c03914a87ac5b4c0ea16e92984
Victor He <victorhe97@gmail.com>
2026-09-02 04:18:51 +0000

    Add tokyonight-storm for color theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
     return (
-        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-                    Hello, World!
-                </div>
+        <div className="flex flex-col flex-1 items-center justify-center font-sans ">
+            <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+                <h1>Hello Victor!</h1>
             </main>
         </div>
     );
